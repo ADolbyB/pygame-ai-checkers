a62f2130005ed51e70ac3ecbc6c1142ee4417f97
Joel Brigida <joel@drtechreps.com>
2022-12-14 22:23:20 -0500

    Minor crash bugfix

--- checkers/board.py
+++ checkers/board.py
@@ -21,7 +21,7 @@ class Board:
         piece.move(row, col)
 
         # if we move and hit the last or 1st row of the board, make piece a KING
-        if row == ROWS or row == 0:
+        if row == ROWS - 1 or row == 0:
             piece.make_king()
             if piece.color == WHITE:
                 self.white_kings += 1
