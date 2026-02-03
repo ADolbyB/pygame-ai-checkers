94b9333b0bc98c8c76f27e89adeb15a20b4c0ce9
Joel Brigida <86583423+ADolbyB@users.noreply.github.com>
2026-02-03 14:18:37 -0500

    fix bug #4: FileNotFound (#5)
    No Conflicts.

--- checkers/constants.py
+++ checkers/constants.py
@@ -1,5 +1,9 @@
 # All constant values are stored in this folder.
 import pygame
+import os   # For PyInstaller
+import sys  # For Pyinstaller
+
+pygame.init()   # Initialize PyGame
 
 WIDTH, HEIGHT = 800, 800 # 800 pixels high by 800 pixels wide
 ROWS, COLS = 8, 8 # 8 rows by 8 columns = std checkers board
@@ -12,4 +16,18 @@ BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 GRAY = (128, 128, 128)
 
-CROWN = pygame.transform.scale(pygame.image.load('checkers/assets/crown.png'), (44, 25))
+# Get correct path for assets (PyInstaller vs. Source code)
+def resource_path(relative_path):
+    try:
+        # PyInstaller creates a temp filder, stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        # If running as source, use normal path
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+CROWN = pygame.transform.scale(
+    pygame.image.load('checkers/assets/crown.png'),
+    (44, 25)
+)
